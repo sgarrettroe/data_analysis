a95a7566e371063eec980e39470a2d2708867381
tbrinzer <tab110@pitt.edu>
2016-01-26 16:43:06 -0500

    Added a numerical high pass filter to allow us to visualize our pump-probe data without the low frequency components.

---
+++ highPassBackend.m
@@ -0,0 +1,19 @@
+function timeSignalMatrix = highPassBackend(PP,t0_bin)
+
+PP_ = PP(:,t0_bin:end); % take the elements at t >= 0
+PP_var = bsxfun(@minus, PP_, mean(PP_,2)); % subtract the mean to avoid a spike in the FT at 0 frequency
+R = sgrsfft(PP_var,[],2); % have to FT along the correct dimension
+% excise the low frequency components
+R(:,1:100) = 0;
+R(:,end-100:end) = 0;
+
+% inverse FFT and take the real part, because we added small imaginary
+% components by excising the low frequency components as we did above
+timeSignalMatrix = real(sgrsifft(R,[],2));
+
+% A = data.PP(:,data.t0_bin:end);
+% A1 = bsxfun(@minus, A, mean(A,2));
+% a = sgrsfft(A1,[],2); % have to FT along the correct dimension
+% a(:,1:100) = 0;
+% a(:,end-100:end) = 0;
+% aprime = real(sgrsifft(a,[],2));
