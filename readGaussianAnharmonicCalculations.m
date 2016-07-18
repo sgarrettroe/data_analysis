fee6b193a52823bff61052f1d2603ca9078040a4
Sean Garrett-Roe <sgarrettroe@gmail.com>
2016-07-18 14:08:36 -0400

    read Gaussian and QChem anharmonic vibrational analysis
    read in the anharmonic coupling constants from Gaussian and QChem

--- readGaussianAnharmonicCalculations.m
+++ readGaussianAnharmonicCalculations.m
@@ -16,9 +16,14 @@ fclose(fid);
 
 %// Search a specific string and find all rows containing matches
 nModesString = '2nd derivatives larger than';
-quadraticString = ':      QUADRATIC FORCE CONSTANTS IN NORMAL MODES       :';
-cubicString = ':        CUBIC FORCE CONSTANTS IN NORMAL MODES         :';
-quarticString = ':       QUARTIC FORCE CONSTANTS IN NORMAL MODES        :';
+%quadraticString = ':      QUADRATIC FORCE CONSTANTS IN NORMAL MODES       :';
+quadraticString = 'QUADRATIC FORCE CONSTANTS IN NORMAL MODES';
+%cubicString = ':        CUBIC FORCE CONSTANTS IN NORMAL MODES         :';
+cubicString = 'CUBIC FORCE CONSTANTS IN NORMAL MODES';
+cubicEnumerateString = '3rd derivatives larger than';
+%quarticString = ':       QUARTIC FORCE CONSTANTS IN NORMAL MODES        :';
+quarticString = 'QUARTIC FORCE CONSTANTS IN NORMAL MODES';
+quarticEnumerateString = '4th derivatives larger than';
 
 indexOfSecondOrderDerivatives = 3;
 indexOfThirdOrderDerivatives = 4;
@@ -35,10 +40,8 @@ thisline = C{1}(nModesStart);
 thisline = thisline{1};
 nmodes = sscanf(thisline,['%i ' nModesString '.*']);
 
-% initialize derivative matrices
+% initialize second derivative vector
 sod = zeros(1,nmodes);
-tod = zeros(nmodes,nmodes,nmodes);
-fod = zeros(nmodes,nmodes,nmodes,nmodes);
 
 % find the start of the quadratic section
 hit = strfind(C{1}, quadraticString);
@@ -58,8 +61,18 @@ end
 % empty or not, if it is empty break, if not process the line taking the
 % coordinates from the first three numbers, assigning that value and looing
 
+%find the start of the Cubic anharmonicity section
 hit = strfind(C{1}, cubicString);
 cubicStart = find(~cellfun('isempty', hit)) + cubicHeaderLength;
+
+%find how many derivatives are listed in this section
+hit = strfind(C{1},cubicEnumerateString);
+thisline =  C{1}(find(~cellfun('isempty', hit)));
+thisline = thisline{1};
+n_tod = sscanf(thisline,['%i ' cubicEnumerateString '.*']);
+
+%initialize tod
+tod = zeros(n_tod,4);
 
 modesDone = false;
 count = 0;
@@ -71,11 +84,12 @@ while ~modesDone
         modesDone = true;
         break
     end
-    stuff = sscanf(thisline,'%i %i %f %f %f');
-    ii = stuff(1);
-    jj = stuff(2);
-    kk = stuff(3);
-    tod(ii,jj,kk) = stuff(indexOfThirdOrderDerivatives);
+    stuff = sscanf(thisline,'%i %i %i %f %f %f');
+%     ii = stuff(1);
+%     jj = stuff(2);
+%     kk = stuff(3);
+%    tod(ii,jj,kk) = stuff(indexOfThirdOrderDerivatives);
+    tod(count,1:4) = [stuff(1:3)' stuff(indexOfThirdOrderDerivatives)];
 end
 
 % lets do the fourth order now by reading the line, looking to see if it is
@@ -84,6 +98,15 @@ end
 
 hit = strfind(C{1}, quarticString);
 quarticStart = find(~cellfun('isempty', hit)) + quarticHeaderLength;
+
+%find how many derivatives are listed in this section
+hit = strfind(C{1},quarticEnumerateString);
+thisline =  C{1}(find(~cellfun('isempty', hit)));
+thisline = thisline{1};
+n_fod = sscanf(thisline,['%i ' quarticEnumerateString '.*']);
+
+%initialize fourth order derivatives matrix
+fod = zeros(n_fod,5);
 
 modesDone = false;
 count = 0;
@@ -95,11 +118,12 @@ while ~modesDone
         modesDone = true;
         break
     end
-    stuff = sscanf(thisline,'%i %i %f %f %f');
-    ii = stuff(1);
-    jj = stuff(2);
-    kk = stuff(3);
-    ll = stuff(4);
-    fod(ii,jj,kk,ll) = stuff(indexOfFourthOrderDerivatives);
+    stuff = sscanf(thisline,'%i %i %i %i %f %f %f');
+%     ii = stuff(1);
+%     jj = stuff(2);
+%     kk = stuff(3);
+%     ll = stuff(4);
+%     fod(ii,jj,kk,ll) = stuff(indexOfFourthOrderDerivatives);
+    fod(count,1:5) = [stuff(1:4)' stuff(indexOfFourthOrderDerivatives)];
 end
 
