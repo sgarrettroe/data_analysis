ba2f1323e9f43fa4a2f5df6ee4f271007dd548ea
tbrinzer <tab110@pitt.edu>
2018-02-14 16:51:14 -0500

    significant update in extractMaxima and estimatePeakArea, fixing several bugs, one instance of bad hard coding, and changing how the options are passed to the function.

--- OO_workspace/extractMaxima.m
+++ OO_workspace/extractMaxima.m
@@ -1,21 +1,62 @@
 function [out] = extractMaxima(dataobj,options)
-% function [out] = extractMaxima(dataobj,startpoint,lb,ub,fitfcn,flag_plot)
-startpoint = options.startpoint;
-lb = options.lb;
-ub = options.ub;
-fitfcn = options.fitfcn;
-flag_plot = options.flag_plot;
+% fitStruct = extractMaxima(dataobj,options)
+%
+% extractMaxima attempts to fit the peak shape of slices of a 2D-IR
+% spectrum for use in centerline slope analysis. Input (dataobj) is assumed
+% to be (cropped) 2D-IR data, which is further cropped for fitting in this
+% function. For each spectrum, this code attempts to fit slices along
+% omega_3 with the inputted fitting function (normally a sum of Gaussians
+% or Voigt profiles). The function returns the data, fit result, goodness
+% of fit, and fitinfo in an n x m structure, where n is the number of t2
+% points, and m is the number of omega_1 values fitted.
+%
+%   required fields for 'options'
+%       'range1' - the omega_1 fitting range
+%       'range3' - the omega_3 fitting range
+%       'fitfcn' - the fitting function
+%       'startpoint' - the starting point of the fit 
+%       'lb' - the lower bound of the fit
+%       'ub' - the upper bound of the fit
+%
+%   possible additional fields
+%       'estimateArea' - (default false) if true, attempts to estimate the
+%       area of the peak by calling 'estimatePeakArea.m', and replaces the
+%       amplitude (assumed format 'a\d') values in the fitting bounds based
+%       on the area. Useful when using a normalized fitting function (like
+%       our Voigt profile) 
+%
+%       'fitPosPeak' - (default false) specifies the behavior of
+%       estimatePeakArea. If true, it will attempt to fit a positive peak
+%       (probably the ESA). If false, it will fit a negative peak (GSB/SE).
+%
+%       'flag_plot' - (default false) If true, plots fitting results as
+%       they come in.
+%
+%       'estimate_flag_plot' - (default false) similar to flag_plot, but
+%       for estimatePeakArea
+       
+flag_plot = 0;
+estimateArea = 0;
+fitPosPeak = 0;
+estimate_flag_plot = 0;
 
-if isfield(options,'estimateArea')
-    estimateArea = options.estimateArea;
-else
-    estimateArea = 0;
+required_fields = {'range1' 'range3' 'fitfcn' 'flag_plot' 'lb' 'startpoint' 'ub'};
+possible_fields = {'estimateArea' 'estimate_flag_plot' 'fitPosPeak' 'fitfcn' 'flag_plot' 'lb' 'startpoint' 'ub'};
+given_fields = fields(options);
+
+missing_fields = setdiff(sort(required_fields),sort(given_fields));
+unexpected_fields = setdiff(sort(given_fields),sort(possible_fields));
+
+if ~isempty(missing_fields)
+    error('Required options fields not found.');
 end
 
-if isfield(options,'fitPosPeak')
-    fitPosPeak = options.fitPosPeak;
-else
-    fitPosPeak = 0;
+if ~isempty(unexpected_fields)
+    warning('Unexpected options fields.')
+end
+
+for ji=1:length(given_fields)
+    eval([given_fields{ji} '=options.' given_fields{ji} ]);
 end
 
 out = struct('fitresult',[],'gof',[],'fitinfo',[]);
@@ -25,15 +66,15 @@ for ii = 1:length(dataobj)
     w3a = min(w3):0.1:max(w3);
     R = dataobj(ii).R;
     for ij = 1:length(w1)
-        % redefining our starting parameters for each spectrum slice. I'm
-        % pretty sure the Voigt profile I'm using is normalized (ie the
-        % total area is 1. So if we can get an estimate of the total area
-        % under one of our curves by a trapezoid rule, that would be spiffy.
+        % redefining our starting parameters for each spectrum slice.
         if estimateArea
-            area = estimatePeakArea(w3(:),R(:,ij),'fitPosPeak',fitPosPeak);
-            startpoint(2:3) = area;
-            lb(2:3) = 0.5*area;
-            ub(2:3) = 2*area;
+            area = estimatePeakArea(w3(:),R(:,ij),'fitPosPeak',fitPosPeak,'flag_plot',estimate_flag_plot);
+            coefficients = coeffnames(fitfcn);
+            aTest = regexp(coefficients,'a\d');
+            ind = ~cellfun(@isempty,aTest);
+            startpoint(ind) = area;
+            lb(ind) = 0.5*area;
+            ub(ind) = 2*area;
         end
         % the fitting part
         [fitresult,gof,fitinfo] = fit(w3(:),R(:,ij),fitfcn, 'StartPoint',startpoint,...
