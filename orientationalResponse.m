19eb046cd893e6277546d27a0e0d759ac343b72f
Sean Garrett-Roe <sgarrettroe@gmail.com>
2018-05-18 13:46:49 -0400

    orientational response
    add perpendicular and crossed polarizations

--- orientationalResponse.m
+++ orientationalResponse.m
@@ -1,17 +1,17 @@
-function r = orientationalResponse(tau_c,order,varargin)
+function [p,q,r] = orientationalResponse(tau_c,order,varargin)
 % orientationalResponse.m Calculate the value of the orientational
 % correlation function for different orders of spectroscopy *for the all
 % parallel case only*!
 %
 % call as r = orientationalResponse(tau_c,order,[times])
 % so for 1st order
-% r = orientationalResponse(tau_c,order,t)
+% [p,q,r] = orientationalResponse(tau_c,order,t)
 %
 % For 3rd order
-% r = orientationalResponse(tau_c,order,T1,t2,T3)
+% [p,q,r] = orientationalResponse(tau_c,3,T1,t2,T3)
 %
-% For 3rd order
-% r = orientationalResponse(tau_c,order,T1,t2,T3,t4,T5)
+% For 5th order
+% [p,q,r] = orientationalResponse(tau_c,5,T1,t2,T3,t4,T5)
 %
 % tau = 1/(6D), i.e. the value from pump-probe anistropy decay measurements
 %   just be careful to use the same units as the time axis/axes
@@ -38,14 +38,17 @@ switch order
     T1 = varargin{1};
     t2 = varargin{2};
     T3 = varargin{3};
-    r = 1/9.*c1(T1).*(1 + 4/5.*c2(t2)).*c1(T3);
-  case 5
+    p = 1/9.*c1(T1).*(1 + 4/5.*c2(t2)).*c1(T3);%parallel
+    q = 1/9.*c1(T1).*(1 - 2/5.*c2(t2)).*c1(T3);%perp
+    r = 1/15.*c1(T1).*c2(t2).*c1(T3);%crossed
+
+    case 5
     T1 = varargin{1};
     t2 = varargin{2};
     T3 = varargin{3};
     t4 = varargin{4};
     T5 = varargin{5};
-    r = c1(T1).*c1(T5).*((1/27)*(1+4/5.*c2(t2)).*(1+4/5.*c2(t4)).*c1(T3)...
+    p = c1(T1).*c1(T5).*((1/27)*(1+4/5.*c2(t2)).*(1+4/5.*c2(t4)).*c1(T3)...
       +4/175*c2(t2)*c2(t4).*c3(T3));
   otherwise
     error(['orientationalResponse.m does not support order ',num2str(order),' only 1,3,5'])
