6bb15f0dd94e4fe7fa6dd0e8a5dd3b6d62c79588
TylerParrack <40776256+TylerParrack@users.noreply.github.com>
2022-05-02 14:16:42 -0400

    added 3exp Lineshape function and edited 2exp lineshape function's c2 calculation

--- OO_spectrum/lsf3exp1fast.m
+++ OO_spectrum/lsf3exp1fast.m
@@ -1,13 +1,13 @@
-classdef lsf2exp1fast < lineshapeFunction
+classdef lsf3exp1fast < lineshapeFunction
     
     properties
-        params = struct('Delta1_cm',[],'tau1',[],'Delta2_cm',[],'tau2',[],'T2',[]);
+        params = struct('Delta1_cm',[],'tau1',[],'Delta2_cm',[],'tau2',[],'Delta3_cm',[],'tau3',[],'T2',[]);
         g;
         c2;
     end
     
     methods
-        function obj = lsf2exp1fast(params)
+        function obj = lsf3exp1fast(params)
             if nargin == 0;
                 super_args = {};
             else
@@ -24,10 +24,13 @@ classdef lsf2exp1fast < lineshapeFunction
             Lambda1 = 1/obj.params(1).tau1;
             Delta2 = obj.params(1).Delta2_cm*wavenumbersToInvPs*2*pi;
             Lambda2 = 1/obj.params(1).tau2;
+            Delta3 = obj.params(1).Delta3_cm*wavenumbersToInvPs*2*pi;
+            Lambda3 = 1/obj.params(1).tau3;
             T2 = obj.params(1).T2;
 
             g = @(t) t./T2 + Delta1^2/Lambda1^2.*(exp(-Lambda1.*t)-1+Lambda1.*t)...
-                + Delta2^2/Lambda2^2.*(exp(-Lambda2.*t)-1+Lambda2.*t);
+                + Delta2^2/Lambda2^2.*(exp(-Lambda2.*t)-1+Lambda2.*t)...
+                + Delta3^2/Lambda3^2.*(exp(-Lambda3.*t)-1+Lambda3.*t);
         end
         
         function c2 = makeC2(obj)
@@ -37,10 +40,13 @@ classdef lsf2exp1fast < lineshapeFunction
             Lambda1 = 1/obj.params(1).tau1;
             Delta2 = obj.params(1).Delta2_cm*wavenumbersToInvPs*2*pi;
             Lambda2 = 1/obj.params(1).tau2;
+            Delta3 = obj.params(1).Delta3_cm*wavenumbersToInvPs*2*pi;
+            Lambda3 = 1/obj.params(1).tau3;
             T2 = obj.params(1).T2;
             
             c2 = @(t) (t==0)/T2 + Delta1^2.*exp(-Lambda1.*t)...
-                + Delta2^2.*exp(-Lambda2.*t);
+                + Delta2^2.*exp(-Lambda2.*t)...
+                + Delta3^2.*exp(-Lambda3.*t);
         end
 
     end
