ccce1e790805d2eba288c53c75bfdae1582bd9f6
Sean Garrett-Roe <sgarrettroe@gmail.com>
2022-05-25 18:54:47 -0400

    move lsf files w/o para

--- OO_spectrum/lsfRISDwobbling1NI.m
+++ OO_spectrum/lsfRISDwobbling1NI.m
@@ -1,93 +1,92 @@
-classdef lsfRISDwobbling1NIpara < lineshapeFunction
+classdef lsfRISDwobbling1NI < lineshapeFunction
     
     properties
-        params = struct('order',[],'tau_o1',[],'theta_deg1',[]);
+        params = struct('Delta1_cm',[],'tr1',[],'theta1_deg',[]);
         g;
         c2;
-
+        order;
         tpoints;
+        L_l;
+        R;
+        pol; %'para' or 'perp'
     end
     
     methods
         
-        function obj = lsfRISDwobbling1NI(params,str,aRFoptions)
-            if nargin == 0;
+        function obj = lsfRISDwobbling1NIpara(params,str,aRFoptions) %constructor function
+            if nargin == 0
                 super_args = {};
-            else
+            elseif nargin == 1 
+                super_args = params; %if we were passed cell array
+                params = super_args{1};
+                str = super_args{2};
+                aRFoptions = super_args{3};
+            elseif nargin == 2
                 super_args{1} = params;
                 super_args{2} = str;
+                aRFoptions = struct([]);
+            elseif naragin == 3
+                super_args{1} = params;
+                super_args{2} = str;
+                super_args{3} = aRFoptions;
+            else
+                error('confusing number of input args in lsfRISDwobbling1NIpara: %i\n',nargin)
             end
             obj@lineshapeFunction(super_args);
-            %obj.tpoints = maketpoints(aRFoptions);
+            if nargin~=0
+                %if we have some input arguments
+                obj.pol = aRFoptions.pol;
+                obj.order = aRFoptions.order;
+                obj = obj.maketpoints(aRFoptions);
+                obj = obj.makeL_l;   
+            end
         end
         
         function out = makeG(obj)
+            global wavenumbersToInvPs;
 
-                    persistent R old_order;     
-                    order = obj.params(1).order;
-                    tr1 = 1/obj.params(1).tau_o1;
-                    theta_deg1 = obj.params(1).theta_deg1;
-
-                            %param struct for R must have these fields tr theta_deg 
-                            p(1).tr = tr1;
-                            p(1).theta_deg = theta_deg1;
-
-                            %Look to see if we need to (re)initialize R
-                            if isempty(R)||isempty(old_order)||old_order~=order
-                                disp('new R');
-
-                                C = wobblingCtest;
-                                Ctot = cell(1,4);
-
-                                for l = 1:4
-                                    %Ctot{l} = 1;
-                                    %for ii = 1:ncones
-                                        Ctot{l}=@(p,t)C{l}(p(1),tau);
-                                    %end
-                                end
-
-                                R = wobblingR(Ctot,order);
-                                old_order=order;
-                            end
-         
-
-            F_para =@(t, tau) (t-tau).*R.para(p,tau); %this is the FFCF
-%             F_perp =@(t) (3/25).*(7.*exp(-2.*D_m.*t) - 2.*exp(-12.*D_m.*t)) ./ (1 - 0.4.*exp(-6.*D_m.*t)); %this is the FFCF
-           
-            g_prime = arrayfun(@(t) integral(@(tau) F_para(t, tau),0,t),obj.tpoints); %do the numerical integration as a function of t
+            tr1 = obj.params(1).tr1;
+            theta1_deg = obj.params(1).theta1_deg;
+            Delta1 = obj.params(1).Delta1_cm*wavenumbersToInvPs*2*pi;
+            
+            %param struct for R must have these fields tr theta_deg
+            p(1).tr1 = tr1;
+            p(1).theta1_deg = theta1_deg;
+            
+            if strcmpi(obj.pol,'para')
+                F =@(t, tau) (t-tau).*Delta1^2.*obj.R.para(tau,p); %this is the FFCF time (t-tau) to turn a double integral into a single one
+                %F_perp =@(t) (3/25).*(7.*exp(-2.*D_m.*t) - 2.*exp(-12.*D_m.*t)) ./ (1 - 0.4.*exp(-6.*D_m.*t)); %this is the FFCF
+            elseif strcmpi(obj.pol,'perp')
+                F =@(t, tau) (t-tau).*Delta1^2.*obj.R.perp(tau,p); %this is the FFCF time (t-tau) to turn a double integral into a single one
+            else
+                error('unknown polarization pol = %s, should be either ''para'' or ''perp''\n',obj.pol);
+            end
+            g_prime = arrayfun(@(t) integral(@(tau) F(t, tau),0,t),obj.tpoints); %do the numerical integration as a function of t
             out = @(t) interp1(obj.tpoints,g_prime,t);
         end
         
         function out = makeC2(obj)
-                 persistent Ctot old_order
-
-            order = obj.params(1).order;
-            tr1 = 1/obj.params(1).tau_o1;
-            theta_deg1 = obj.params(1).theta_deg1;
             
-                         p(1).tr = tr1;
-                         p(1).theta_deg = theta_deg1;
-            %Look to see if we need to (re)initialize R
-                if isempty(Ctot)||isempty(old_order)||old_order~=order
-                    disp('new Ctot');
-
-                    C = wobblingC;
-                    Ctot = cell(1,4);
-
-
-                    for l = 1:4
-                        %Ctot{l} = 1;
-                        %for ii = 1:ncones
-                            Ctot{l}=@(p,t)C{l}(p(1),t);
-                        %end
-                    end
-                old_order=order;
-                end 
-                
+            tr = 1/obj.params(1).tr;
+            theta_deg = obj.params(1).theta_deg;
+            
+            p(1).tr = tr;
+            p(1).theta_deg = theta_deg;
+            
+            C = wobblingCv2;
+            Ctot = cell(1,4);
+            
+            for l = 1:4
+                %Ctot{l} = 1;
+                %for ii = 1:ncones
+                Ctot{l}=@(p,t)C{l}(p(1),t);
+                %end
+            end
+            
             out = @(t) 0.4.*(Ctot{2}(p,t2));
             
         end
-    
+        
         function obj = maketpoints(obj,aRFoptions)
             t1 = 0:aRFoptions.dt:(aRFoptions.n_t-1)*aRFoptions.dt;
             t3 = t1;
@@ -100,6 +99,21 @@ classdef lsfRISDwobbling1NIpara < lineshapeFunction
             obj.tpoints = unique([tmp,tmp2]);
             
         end
+        function obj = makeL_l(obj)
+%            C = wobblingCtest;
+            C = wobblingCv2;
+            Ctot = cell(1,4);            
+            for l = 1:4
+                %Ctot{l} = 1;
+                %for ii = 1:ncones
+                Ctot{l}=@(tau,p)C{l}(tau,p.tr1,p.theta1_deg);
+                %end
+            end
+            
+            obj.R = wobblingRv2(Ctot,obj.order);
+            obj.L_l = Ctot;
+        end
+
     end
 end
 
--- OO_spectrum/lsfRISDwobbling1NIBnd.m
+++ OO_spectrum/lsfRISDwobbling1NIBnd.m
@@ -1,21 +1,18 @@
 classdef lsfRISDwobbling1NIBnd < lsfRISDwobbling1NI & lineshapeFunctionBnd
-   properties
-       
-   end
-   
-   methods
-       function obj = lsfRISDwobbling1NIBnd(params,str,aRFoptions)
-            if nargin == 0;
+    properties
+        
+    end
+    
+    methods
+        function obj = lsfRISDwobbling1NIparaBnd(params,str,aRFoptions)
+            if nargin == 0
                 super_args = {};
             else
                 super_args{1} = params;
                 super_args{2} = str;
                 super_args{3} = aRFoptions;
             end
-            obj@lineshapeFunctionBnd(super_args);
-            obj = obj.maketpoints(aRFoptions);
+            obj@lsfRISDwobbling1NIpara(super_args);
         end
- 
-   end
-   
+    end
 end
