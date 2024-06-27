9d14386fabc2a7e8fd85ff14904f5bebfcdb886e
Sean Garrett-Roe <sgarrettroe@gmail.com>
2024-06-27 09:04:13 +0100

    Update my2dPlot.m
    add flag to turn off projections

--- my2dPlot.m
+++ my2dPlot.m
@@ -4,9 +4,9 @@ function varargout=my2dPlot(x,y,z,varargin)
 %returning handles to the individual axes that make the plot
 %function varargout=my2dPlot(x,y,z,['opt',val])
 %
-%RB, 03052011: introduced variable zlimit, which changes the range over 
-%which the contours are drawn. zlimit <= 0 is default and uses the data, 
-%zlimit > 0 and <= 1 will plot a ratio, zlimit > 0 limits the contours to 
+%RB, 03052011: introduced variable zlimit, which changes the range over
+%which the contours are drawn. zlimit <= 0 is default and uses the data,
+%zlimit > 0 and <= 1 will plot a ratio, zlimit > 0 limits the contours to
 %the range specified.
 %
 %fix contour levels and colors
@@ -17,30 +17,39 @@ zlimit = 0;
 mask = ones(size(z));
 mask_level = [];
 mask_from = [];
+flag_projections = true;
 while length(varargin)>=2
-  arg = varargin{1};
-  val = varargin{2};
-  switch lower(arg)
-    case 'zlimit'
-      zlimit = val;  
-    case 'n_contours'
-      n_contours = val;
-      if mod(n_contours,2)
-        warning('my2dPlot4: Odd number of contour lines may produce unexpected results!')
-      end
-    case {'pumpprobe_style','pumpprobe'}
-      flag_pumpprobe = val;
-    case 'mask'
-      mask = val;
-    case 'mask_level'
-          mask_level = val;
-    case 'mask_from'
-          mask_from = val;
-          if size(mask_from)~=size(z),error('mask_from size must match input size');end
-    otherwise
-      error(['my2dPlot: unknown option ',arg])
-  end
-  varargin = varargin(3:end);
+    arg = varargin{1};
+    val = varargin{2};
+    switch lower(arg)
+        case 'zlimit'
+            zlimit = val;
+        case 'n_contours'
+            n_contours = val;
+            if mod(n_contours,2)
+                warning('my2dPlot4: Odd number of contour lines may produce unexpected results!')
+            end
+        case {'pumpprobe_style','pumpprobe'}
+            flag_pumpprobe = val;
+        case 'mask'
+            mask = val;
+        case 'mask_level'
+            mask_level = val;
+        case 'mask_from'
+            mask_from = val;
+            if size(mask_from)~=size(z),error('mask_from size must match input size');end
+        case 'projections'
+            if strcmpi(val,'on')
+                flag_projections = true;
+            elseif strcmpi(val,'off')
+                flag_projections = false;
+            elseif islogical(val)
+                flag_projections=val;
+            end
+        otherwise
+            error(['my2dPlot: unknown option ',arg])
+    end
+    varargin = varargin(3:end);
 end
 
 if ~isempty(mask_level) & ~isempty(mask_from)
@@ -51,7 +60,7 @@ z = z.*mask;
 
 pos = get(gcf,'Position');
 if get(gcf,'WindowStyle')~='docked',
-  set(gcf,'Position',[pos(1) pos(2) 415 416],'Color',[0.9 0.9 0.9]);
+    set(gcf,'Position',[pos(1) pos(2) 415 416],'Color',[0.9 0.9 0.9]);
 end
 %contour properties
 cont_left = 0.18;
@@ -72,28 +81,28 @@ map=myMapRGB2(n_contours);
 % and default). If zlimit is between 0 and 1, it will use the data as well,
 % but only plots a fraction of it (the number between 0 and 1. If zlimit is
 % large than 1, it will use that value.
-if zlimit <= 0 
-  [ca, level_list]= myCaxis2(z, n_contours);
-  %ca 
+if zlimit <= 0
+    [ca, level_list]= myCaxis2(z, n_contours);
+    %ca
 elseif zlimit > 0 && zlimit <= 1
-  [ca, level_list] = myCaxis2(z, n_contours);
-  ca = ca * zlimit;
-  level_list = level_list * zlimit;
+    [ca, level_list] = myCaxis2(z, n_contours);
+    ca = ca * zlimit;
+    level_list = level_list * zlimit;
 else
-  ca = [-zlimit zlimit];
-  level_list = linspace(-zlimit, zlimit, n_contours+2);
+    ca = [-zlimit zlimit];
+    level_list = linspace(-zlimit, zlimit, n_contours+2);
 end
-  
-  
+
+
 contourf(x,y,z,level_list);
 colormap(map)
 caxis(ca);
 if flag_pumpprobe
-  x_label = '\omega_{probe} / 2\pic';
-  y_label = '\omega_{pump} / 2\pic';
+    x_label = '\omega_{probe} / 2\pic';
+    y_label = '\omega_{pump} / 2\pic';
 else
-  x_label = '\omega_1 / 2\pic';
-  y_label = '\omega_3 / 2\pic';
+    x_label = '\omega_1 / 2\pic';
+    y_label = '\omega_3 / 2\pic';
 end
 xlabel(x_label);
 ylabel(y_label);
@@ -105,27 +114,29 @@ line([x(1) x(end)],[x(1) x(end)],'Color',[0 0 0]);
 xlim = get(a(1),'XLim');
 ylim = get(a(1),'YLim');
 
-a(2)=axes('Position',[cont_left, cont_bot+cont_h, cont_w, proj_h]);
-%line(x,sum(z,2)','Color',[0 0 0]);
-line(x,sum(z,1),'Color',[0 0 0]);
-set(a(2),'XTickLabel',[],'YTickLabel',[],'Xlim',xlim);
+if flag_projections
+    a(2)=axes('Position',[cont_left, cont_bot+cont_h, cont_w, proj_h]);
+    %line(x,sum(z,2)','Color',[0 0 0]);
+    line(x,sum(z,1),'Color',[0 0 0]);
+    set(a(2),'XTickLabel',[],'YTickLabel',[],'Xlim',xlim);
 
-a(3)=axes('Position',[cont_left+cont_w, cont_bot, proj_h, cont_h]);
-line(sum(z,2)',y,'Color',[0 0 0]);
-set(a(3),'XTickLabel',[],'YTickLabel',[],'YLim',ylim);
+    a(3)=axes('Position',[cont_left+cont_w, cont_bot, proj_h, cont_h]);
+    line(sum(z,2)',y,'Color',[0 0 0]);
+    set(a(3),'XTickLabel',[],'YTickLabel',[],'YLim',ylim);
 
-axes(a(2))
+    axes(a(2))
 
-%linkaxes([a(2) a(1)],'x')
-%linkaxes([a(3) a(1)],'y')
+    %linkaxes([a(2) a(1)],'x')
+    %linkaxes([a(3) a(1)],'y')
 
-hlink_x = linkprop([a(1),a(2)],'XLim');
-hlink_y = linkprop([a(1),a(3)],'YLim');
-key_x = 'graphics_linkprop';
-key_y = 'graphics_linkprop';
+    hlink_x = linkprop([a(1),a(2)],'XLim');
+    hlink_y = linkprop([a(1),a(3)],'YLim');
+    key_x = 'graphics_linkprop';
+    key_y = 'graphics_linkprop';
 
-setappdata(a(2),key_x,hlink_x);
-setappdata(a(3),key_y,hlink_y);
+    setappdata(a(2),key_x,hlink_x);
+    setappdata(a(3),key_y,hlink_y);
+end
 for i = 1:nargout
-  varargout{i} = a(i);
+    varargout{i} = a(i);
 end
